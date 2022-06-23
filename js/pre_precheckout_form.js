1110b485508d1bac0f495eeafe4688827eb85b97
florianverdonck <florianverdonck@gmail.com>
2022-06-23 17:43:23 +0200

    no prevent default

--- js/pre_precheckout_form.js
+++ js/pre_precheckout_form.js
@@ -1,8 +1,8 @@
-$("#order-checkout-form").on("submit", function (e) {
-    console.log("submitted");
-    $("#lead_submit").addClass("disabled");
-    $("#lead_submit").prop("value", "Chargement en cours...");
-    $(".loading-before-checkout").removeClass("d-none");
-    //prevent default behaviour
-    e.preventDefault();
+$(document).ready(function () {
+    $("#order-checkout-form").on("submit", function (e) {
+        console.log("submitted");
+        $("#lead_submit").addClass("disabled");
+        $("#lead_submit").prop("value", "Chargement en cours...");
+        $(".loading-before-checkout").removeClass("d-none");
+    });
 });
