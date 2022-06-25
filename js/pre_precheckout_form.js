210fe95cdbf940913b8dbd9ade56e08a8906658c
florianverdonck <florianverdonck@gmail.com>
2022-06-25 15:01:46 +0200

    fill hidden metadata fields

--- js/pre_precheckout_form.js
+++ js/pre_precheckout_form.js
@@ -4,6 +4,8 @@ $(document).ready(function () {
         $("#lead_submit").addClass("disabled");
         $("#lead_submit").prop("value", "Chargement en cours...");
         $(".loading-before-checkout").removeClass("d-none");
+        $("#lead_model").prop("value", selectedModel);
+        $("#lead_language").prop("value", Weglot.getCurrentLang());
     });
 
     let floatingArrow = '<div><a href="#order-form"><i class="fas fa-arrow-down fa-3x floating mt-3"></i></a></div>';
