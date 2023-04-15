723f0f50f56d2544b9520fe5e53d3159ef2e1eed
florianverdonck <florianverdonck@gmail.com>
2023-04-15 12:03:44 +0200

    move local storage getters/setters to general

--- js/general.js
+++ js/general.js
@@ -16,3 +16,27 @@ function autoScrollTo(el) {
     var top = $("#" + el).offset().top - 100;
     $("html, body").animate({ scrollTop: top }, 500);
 }
+
+function storeVehicleDetail(infoType, infoValue) {
+    console.log(`Storing ${infoType} = ${infoValue}`)
+    localStorage.setItem(infoType, infoValue);
+}
+
+function getVehicleDetail(infoType) {
+    return localStorage.getItem(infoType);
+}
+
+function getPrice(model, year) {
+    switch (year + "_" + model) {
+        case "PRE2016_T45":
+            return 499; 
+        case "PRE2016_T80":
+            return 499; 
+        case "POST2016_T45":
+            return 1199; 
+        case "POST2016_T80":
+            return 1199; 
+        default:
+            return 0;
+    }
+}
