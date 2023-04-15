7912e80c74183b21014d863ecfd3e3b338184161
florianverdonck <florianverdonck@gmail.com>
2023-04-15 11:45:11 +0200

    added facebook addToCart event

--- js/car_selection.js
+++ js/car_selection.js
@@ -106,3 +106,18 @@ function storeVehicleDetail(infoType, infoValue) {
 function getVehicleDetail(infoType) {
     return localStorage.getItem(infoType);
 }
+
+function getPrice(model, year) {
+    switch (year + "_" + model) {
+        case "PRE2016_T45":
+            return 499; 
+        case "PRE2016_T80":
+            return 499; 
+        case "POST2016_T45":
+            return 1199; 
+        case "POST2016_T80":
+            return 1199; 
+        default:
+            return 0;
+    }
+}
--- js/pre_precheckout_form.js
+++ js/pre_precheckout_form.js
@@ -1,11 +1,24 @@
 $(document).ready(function () {
+
+    let model = getVehicleDetail("year");
+    let year = getVehicleDetail("model");
+    let price = getPrice(model, year);
+
     $("#order-checkout-form").on("submit", function (e) {
         console.log("submitted");
         $("#lead_submit").addClass("disabled");
         $("#lead_submit").prop("value", "Chargement en cours...");
         $(".loading-before-checkout").removeClass("d-none");
-        $("#lead_model").prop("value", selectedModel);
+        $("#lead_model").prop("value", model);
         $("#lead_language").prop("value", Weglot.getCurrentLang());
+
+        fbq('track', 'AddToCart', {
+            value: price,
+            currency: 'EUR',
+            content_ids: [ `${year}_${model}` ],
+            content_type: 'product'
+        });
+
     });
 
     let floatingArrow = '<div><a href="#order-form"><i class="fas fa-arrow-down fa-3x floating mt-3"></i></a></div>';
