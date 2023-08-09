2587e39b2cb430ab52038b167ba9a085790179ab
florianverdonck <florianverdonck@gmail.com>
2023-08-09 23:08:08 +0200

    Adds cypress testing

---
+++ cypress.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+      
+    },
+    viewportWidth: 1280,
+    viewportHeight: 800,
+    experimentalModifyObstructiveThirdPartyCode: true,
+  },
+};
